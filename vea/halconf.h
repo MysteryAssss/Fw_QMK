ca5af4c2b38f3647aed8f2d957369de9d602b27e
MysteryAssss <88171916+MysteryAssss@users.noreply.github.com>
2024-10-01 05:31:20 +0700

    Add files via upload

---
+++ vea/halconf.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define HAL_USE_SERIAL TRUE
+#define HAL_USE_I2C TRUE
+#define SERIAL_USB_BUFFERS_SIZE 256
+#define PAL_USE_CALLBACKS TRUE
+#define HAL_USE_SPI TRUE
+
+#include_next <halconf.h>
---
+++ vea/mcuconf.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef STM32_PWM_USE_TIM16
+#define STM32_PWM_USE_TIM16 TRUE
+
+#undef STM32_SERIAL_USE_USART1
+#define STM32_SERIAL_USE_USART1 TRUE
+
+#undef STM32_I2C_USE_I2C1
+#define STM32_I2C_USE_I2C1 FALSE
+
+#undef STM32_I2C_USE_I2C2
+#define STM32_I2C_USE_I2C2 TRUE
+
+#undef STM32_SPI_USE_SPI1
+#define STM32_SPI_USE_SPI1 TRUE
+
+// // Define DMA streams for I2C2
+// #define STM32_I2C_I2C2_RX_DMA_STREAM STM32_DMA_STREAM_ID(1, 5) // DMA1 Channel 5
+// #define STM32_I2C_I2C2_TX_DMA_STREAM STM32_DMA_STREAM_ID(1, 4) // DMA1 Channel 4
+
+// #define STM32_DMA_REQUIRED TRUE
