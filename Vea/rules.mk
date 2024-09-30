13f1a1a47f95372eaf382d7721fae5374aa37f5d
MysteryAssss <88171916+MysteryAssss@users.noreply.github.com>
2024-10-01 05:07:04 +0700

    Add files via upload

---
+++ Vea/rules.mk
@@ -0,0 +1,23 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+CONSOLE_ENABLE = yes
+COMMAND_ENABLE = yes
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+NKRO_ENABLE = yes
+OLED_ENABLE = yes
+WPM_ENABLE = yes			# WPM
+OLED_ENABLE = yes			# Oled
+OLED_TRANSPORT = i2c
+SPLIT_KEYBOARD = yes
+VIA_ENABLE = yes
+SERIAL_DRIVER = usart
+KEYBOARD_SHARED_EP = yes
+I2C_DRIVER_REQUIRED = yes
+SERIAL_DRIVER = bitbang
+
+
